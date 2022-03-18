c641196d04793c91186bf39f54c0bb4d9fa63f1b
marco-vit-pagopa <marco.vit@pagopa.it>
2022-03-18 14:50:04 +0100

    Aggiunto main

---
+++ src/main/java/it/pagopa/pn/external/registries/PnExternalRegistriesApplication.java
@@ -0,0 +1,23 @@
+package it.pagopa.pn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PnExternalRegistriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnExternalRegistriesApplication.class, args);
+	}
+
+	@RestController
+	public static class HomeController {
+
+		@GetMapping("")
+		public String home() {
+			return "Sono Vivo";
+		}
+	}
+}
