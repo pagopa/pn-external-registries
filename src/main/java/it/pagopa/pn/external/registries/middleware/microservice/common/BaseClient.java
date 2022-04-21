9ec5f6b3d1aa2c95ebe65ae98890d07fe2ba429c
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-04-21 11:02:48 +0200

    sviluppi classe baseclient

---
+++ src/main/java/it/pagopa/pn/external/registries/middleware/microservice/common/BaseClient.java
@@ -0,0 +1,75 @@
+package it.pagopa.pn.external.registries.middleware.microservice.common;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import it.pagopa.pn.external.registries.generated.openapi.client.v1.ApiClient;
+import it.pagopa.pn.external.registries.pdnd.service.AccessTokenCacheService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+
+public abstract class BaseClient {
+
+    private final AccessTokenCacheService accessTokenCacheService;
+    private final String purposeId;
+    private final ApiClient apiClient;
+
+    protected  BaseClient(AccessTokenCacheService accessTokenCacheService, String purposeId, String basepath){
+        this.accessTokenCacheService = accessTokenCacheService;
+        this.purposeId = purposeId;
+
+        HttpClient httpClient = HttpClient.create().option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000)
+                .doOnConnected(connection -> connection.addHandlerLast(new ReadTimeoutHandler(1000, TimeUnit.MILLISECONDS)));
+
+        WebClient webClient = ApiClient.buildWebClientBuilder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .filters(filterList -> {
+                    filterList.add(bearerAuthFilter());
+                    filterList.add(renewTokenFilter());
+                })
+                .build();
+        apiClient = new ApiClient(webClient);
+        apiClient.setBasePath(basepath);
+    }
+
+    protected ApiClient getApiClient(){
+        return this.apiClient;
+    }
+
+    private ExchangeFilterFunction bearerAuthFilter() {
+        return (request, next) -> {
+            ClientRequest filtered = ClientRequest.from(request)
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessTokenCacheService.getToken(this.purposeId).block(Duration.ofMillis(3000)))
+                    .build();
+            return next.exchange(filtered);
+        };
+    }
+
+    private ExchangeFilterFunction renewTokenFilter() {
+        return (request, next) -> next.exchange(request).flatMap(response -> {
+            if (response.statusCode().value() == HttpStatus.UNAUTHORIZED.value()) {
+                return response.releaseBody()
+                        .then(accessTokenCacheService.getToken(this.purposeId))
+                        .flatMap(token -> {
+                            ClientRequest newRequest = ClientRequest.from(request)
+                                    .headers(headers -> headers.remove(HttpHeaders.AUTHORIZATION))
+                                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                                    .build();
+                            return next.exchange(newRequest);
+                        });
+            } else {
+                return Mono.just(response);
+            }
+        });
+    }
+
+}
