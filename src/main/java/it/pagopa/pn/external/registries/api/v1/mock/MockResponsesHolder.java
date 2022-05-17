1e9eeab4127b60ff710f1105ada43a36a747a2d1
marco-vit-pagopa <marco.vit@pagopa.it>
2022-05-17 18:00:27 +0200

    PN-1051 dati mock opzionali

--- src/main/java/it/pagopa/pn/external/registries/api/v1/mock/MockResponsesHolder.java
+++ src/main/java/it/pagopa/pn/external/registries/api/v1/mock/MockResponsesHolder.java
@@ -1,27 +1,19 @@
 package it.pagopa.pn.external.registries.api.v1.mock;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import it.pagopa.pn.external.registries.exceptions.InternalErrorException;
-import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.dto.PaContactsDto;
-import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.dto.PaInfoDto;
-import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.dto.PaSummaryDto;
-import it.pagopa.pn.external.registries.generated.openapi.server.recipient.domicile.v1.dto.AnalogDomicileDto;
-import it.pagopa.pn.external.registries.generated.openapi.server.recipient.domicile.v1.dto.DigitalDomicileDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 @Slf4j
 @Component
@@ -32,15 +24,17 @@ public class MockResponsesHolder {
     private Resource mockResource;
     private MockResponses mockData;
 
-    public MockResponsesHolder(ApplicationContext ctx,  @Value("${pn.external-registry.mock-data-resources}") String mockResourceUri) {
+    public MockResponsesHolder(ApplicationContext ctx,  @Value("${pn.external-registry.mock-data-resources:}") String mockResourceUri) {
         this.ctx = ctx;
         this.mockResourceUri = mockResourceUri;
     }
 
     @PostConstruct
     public void init() {
-        this.mockResource = ctx.getResource( mockResourceUri );
-        this.mockData = loadMockResponses( this.mockResource );
+        if( StringUtils.hasText( this.mockResourceUri) ) {
+            this.mockResource = ctx.getResource( mockResourceUri );
+            this.mockData = loadMockResponses( this.mockResource );
+        }
     }
 
     public MockResponses getMockData() {
