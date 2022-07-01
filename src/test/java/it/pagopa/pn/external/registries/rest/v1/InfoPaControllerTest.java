d32841aa13a877da59bb9681c60ad43ebe889b55
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-07-01 17:03:25 +0200

    PN-1730: fix test cov

--- src/test/java/it/pagopa/pn/external/registries/rest/v1/InfoPaControllerTest.java
+++ src/test/java/it/pagopa/pn/external/registries/rest/v1/InfoPaControllerTest.java
@@ -127,7 +127,7 @@ class InfoPaControllerTest {
         res.add(dto);
 
         // When
-        Mockito.when(svc.getGroups(Mockito.any(), Mockito.any(), Mockito.any(), null))
+        Mockito.when(svc.getGroups(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any()))
                 .thenReturn(Flux.fromIterable(res));
 
 
