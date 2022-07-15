07e36d8b03e74725854b227d8f19a1a4a0dc7ef1
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-07-15 14:08:04 +0200

    PN-1875: implementazione Dao

---
+++ src/main/java/it/pagopa/pn/external/registries/middleware/db/io/entities/OptInSentEntity.java
@@ -0,0 +1,35 @@
+package it.pagopa.pn.external.registries.middleware.db.io.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.UpdateBehavior;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
+
+import java.time.Instant;
+
+@EqualsAndHashCode
+public class OptInSentEntity {
+
+    public static final String COL_PK = "pk";
+    private static final String COL_CREATED = "created";
+    private static final String COL_LAST_MODIFIED = "lastModified";
+
+    public OptInSentEntity(String hashedTaxId){
+        this.pk = hashedTaxId;
+        this.setCreated(Instant.now());
+        this.setLastModified(this.getCreated());
+    }
+
+    @DynamoDbIgnore
+    public String getHashedTaxId(){
+        return this.pk;
+    }
+
+
+    @Setter @Getter(onMethod=@__({@DynamoDbPartitionKey, @DynamoDbAttribute(COL_PK)}))  private String pk;
+
+
+    @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_CREATED), @DynamoDbUpdateBehavior(UpdateBehavior.WRITE_IF_NOT_EXISTS)}))  private Instant created;
+    @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_LAST_MODIFIED)}))  private Instant lastModified;
+}
