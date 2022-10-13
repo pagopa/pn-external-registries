1bd1c2de7357c49a3c8ff5b8b05918093d398fae
Antonio Pezzuto <antonio.pezzuto@pagopa.it>
2022-10-13 11:26:40 +0200

    [PN-2198] Fix test

--- src/test/java/it/pagopa/pn/external/registries/services/InfoSelfcareServiceMockTest.java
+++ src/test/java/it/pagopa/pn/external/registries/services/InfoSelfcareServiceMockTest.java
@@ -1,5 +1,20 @@
 package it.pagopa.pn.external.registries.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.ActiveProfiles;
 import it.pagopa.pn.external.registries.api.v1.mock.InfoPapiImpl;
 import it.pagopa.pn.external.registries.exceptions.PnPANotFoundException;
 import it.pagopa.pn.external.registries.generated.openapi.selfcare.external.client.v1.dto.PageOfUserGroupResourceDto;
@@ -11,225 +26,212 @@ import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.dto.PaSu
 import it.pagopa.pn.external.registries.middleware.msclient.SelfcareInstitutionsClient;
 import it.pagopa.pn.external.registries.middleware.msclient.SelfcareUserGroupClient;
 import it.pagopa.pn.external.registries.middleware.queue.producer.sqs.SqsNotificationPaidProducer;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.test.context.ActiveProfiles;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {InfoSelfcareServiceMock.class})
 @ActiveProfiles("test")
 class InfoSelfcareServiceMockTest {
 
-    private final Duration d = Duration.ofMillis(3000);
+  private final Duration d = Duration.ofMillis(3000);
 
-    @Autowired
-    private InfoSelfcareServiceMock service;
+  @Autowired
+  private InfoSelfcareServiceMock service;
 
-    @MockBean
-    private SelfcareInstitutionsClient selfcareInstitutionsClient;
+  @MockBean
+  private SelfcareInstitutionsClient selfcareInstitutionsClient;
 
-    @MockBean
-    private SelfcareUserGroupClient selfcareUserGroupClient;
+  @MockBean
+  private SelfcareUserGroupClient selfcareUserGroupClient;
 
-    @MockBean
-    private InfoPapiImpl infoPapi;
+  @MockBean
+  private InfoPapiImpl infoPapi;
 
-    @Configuration
-    static class ContextConfiguration {
-        @Primary
-        @Bean
-        public SqsNotificationPaidProducer sqsNotificationPaidProducer() {
-            return Mockito.mock( SqsNotificationPaidProducer.class);
-        }
+  @Configuration
+  static class ContextConfiguration {
+    @Primary
+    @Bean
+    public SqsNotificationPaidProducer sqsNotificationPaidProducer() {
+      return Mockito.mock(SqsNotificationPaidProducer.class);
     }
+  }
 
-    @Test
-    void getOnePa() {
-        //GIVEN
-        String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
-        PaInfoDto inst = new PaInfoDto();
-        inst.setId(id);
-        inst.setName("Comune di Milano");
+  @Test
+  void getOnePa() {
+    // GIVEN
+    String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
+    PaInfoDto inst = new PaInfoDto();
+    inst.setId(id);
+    inst.setName("Comune di Milano");
 
-        Mockito.when(infoPapi.getOnePa(Mockito.anyString())).thenReturn(Mono.just(inst));
+    Mockito.when(infoPapi.getOnePa(Mockito.anyString())).thenReturn(Mono.just(inst));
 
-        // WHEN
-        PaInfoDto res = service.getOnePa(id).block();
-
-
-        //THEN
-        assertNotNull(res);
-        assertEquals("Comune di Milano", res.getName());
-        assertEquals(id, res.getId());
-    }
-
-    @Test
-    void getOnePaNotExist() {
-        //GIVEN
-        String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
-        Mockito.when(infoPapi.getOnePa(Mockito.anyString())).thenReturn(Mono.error(new PnPANotFoundException()));
+    // WHEN
+    PaInfoDto res = service.getOnePa(id).block();
 
 
-        // WHEN
-        Mono<PaInfoDto> mono =service.getOnePa(id);
-        assertThrows(PnPANotFoundException.class, () -> mono.block(d));
-        //THEN
-    }
+    // THEN
+    assertNotNull(res);
+    assertEquals("Comune di Milano", res.getName());
+    assertEquals(id, res.getId());
+  }
 
-    @Test
-    void listOnboardedPaByName() {
-        //GIVEN
-        String name = "comune";
-        String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
-        PaSummaryDto inst = new PaSummaryDto();
-        inst.setId(id);
-        inst.setName("Comune di Milano");
-
-        List<PaSummaryDto> list = new ArrayList<>();
-        list.add(inst);
-
-        Mockito.when(infoPapi.listOnboardedPaByName(name)).thenReturn(Flux.fromIterable(list));
+  @Test
+  void getOnePaNotExist() {
+    // GIVEN
+    String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
+    Mockito.when(infoPapi.getOnePa(Mockito.anyString()))
+        .thenReturn(Mono.error(new PnPANotFoundException()));
 
 
-        // WHEN
-        List<PaSummaryDto> res = service.listOnboardedPaByName(name).collectList().block();
+    // WHEN
+    Mono<PaInfoDto> mono = service.getOnePa(id);
+    assertThrows(PnPANotFoundException.class, () -> mono.block(d));
+    // THEN
+  }
+
+  @Test
+  void listOnboardedPaByName() {
+    // GIVEN
+    String name = "comune";
+    String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
+    PaSummaryDto inst = new PaSummaryDto();
+    inst.setId(id);
+    inst.setName("Comune di Milano");
+
+    List<PaSummaryDto> list = new ArrayList<>();
+    list.add(inst);
+
+    Mockito.when(infoPapi.listOnboardedPaByName(name)).thenReturn(Flux.fromIterable(list));
 
 
-        //THEN
-        assertNotNull(res);
-        assertEquals("Comune di Milano", res.get(0).getName());
-    }
-
-    @Test
-    void listOnboardedPaByIds() {
-        //GIVEN
-        String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
-        PaSummaryDto inst = new PaSummaryDto();
-        inst.setId(id);
-        inst.setName("Comune di Milano");
-
-        List<PaSummaryDto> list = new ArrayList<>();
-        list.add(inst);
-
-        Mockito.when(infoPapi.listOnboardedPaByIds(List.of(id))).thenReturn(Flux.fromIterable(list));
-
-        // WHEN
-        List<PaSummaryDto> res = service.listOnboardedPaByIds(List.of(id)).collectList().block();
+    // WHEN
+    List<PaSummaryDto> res = service.listOnboardedPaByName(name).collectList().block();
 
 
-        //THEN
-        assertNotNull(res);
-        assertEquals("Comune di Milano", res.get(0).getName());
-    }
+    // THEN
+    assertNotNull(res);
+    assertEquals("Comune di Milano", res.get(0).getName());
+  }
+
+  @Test
+  void listOnboardedPaByIds() {
+    // GIVEN
+    String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
+    PaSummaryDto inst = new PaSummaryDto();
+    inst.setId(id);
+    inst.setName("Comune di Milano");
+
+    List<PaSummaryDto> list = new ArrayList<>();
+    list.add(inst);
+
+    Mockito.when(infoPapi.listOnboardedPaByIds(List.of(id))).thenReturn(Flux.fromIterable(list));
+
+    // WHEN
+    List<PaSummaryDto> res = service.listOnboardedPaByIds(List.of(id)).collectList().block();
 
 
-    @Test
-    void getGroups() {
-        //GIVEN
-        String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
-        UserGroupResourceDto inst = new UserGroupResourceDto();
-        inst.setId(id);
-        inst.setName("gruppo1");
-        inst.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
+    // THEN
+    assertNotNull(res);
+    assertEquals("Comune di Milano", res.get(0).getName());
+  }
 
 
-        List<UserGroupResourceDto> list = new ArrayList<>();
-        list.add(inst);
-
-        PageOfUserGroupResourceDto response = new PageOfUserGroupResourceDto();
-        response.setContent(list);
-
-        Mockito.when(selfcareUserGroupClient.getUserGroups(id)).thenReturn(Mono.just(response));
-
-        // WHEN
-        List<PaGroupDto> res = service.getGroups(id,id,null, null).collectList().block();
+  @Test
+  void getGroups() {
+    // GIVEN
+    String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
+    UserGroupResourceDto inst = new UserGroupResourceDto();
+    inst.setId(id);
+    inst.setName("gruppo1");
+    inst.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
 
 
-        //THEN
-        assertNotNull(res);
-        assertEquals(inst.getName(), res.get(0).getName());
-    }
+    List<UserGroupResourceDto> list = new ArrayList<>();
+    list.add(inst);
 
-    @Test
-    void getGroupsFiltered() {
-        //GIVEN
-        String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
-        List<UserGroupResourceDto> list = new ArrayList<>();
-        UserGroupResourceDto inst1 = new UserGroupResourceDto();
-        inst1.setId(id);
-        inst1.setName("gruppo1");
-        inst1.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
-        list.add(inst1);
+    PageOfUserGroupResourceDto response = new PageOfUserGroupResourceDto();
+    response.setContent(list);
 
-        UserGroupResourceDto inst2 = new UserGroupResourceDto();
-        inst2.setId(id+"2");
-        inst2.setName("gruppo2");
-        inst2.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
-        list.add(inst2);
+    Mockito.when(selfcareUserGroupClient.getUserGroups(id)).thenReturn(Mono.just(response));
 
-        PageOfUserGroupResourceDto response = new PageOfUserGroupResourceDto();
-        response.setContent(list);
-
-        Mockito.when(selfcareUserGroupClient.getUserGroups(id)).thenReturn(Mono.just(response));
-
-        // WHEN
-        List<PaGroupDto> res = service.getGroups(id,id,List.of(id), null).collectList().block();
+    // WHEN
+    List<PaGroupDto> res = service.getGroups(id, id, null, null).collectList().block();
 
 
-        //THEN
-        assertNotNull(res);
-        assertEquals(1, res.size());
-        assertEquals(inst1.getName(), res.get(0).getName());
-    }
+    // THEN
+    assertNotNull(res);
+    assertEquals(0, res.size());
+  }
 
-    @Test
-    void getGroupsFilteredActive() {
-        //GIVEN
-        String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
-        List<UserGroupResourceDto> list = new ArrayList<>();
-        UserGroupResourceDto inst1 = new UserGroupResourceDto();
-        inst1.setId(id);
-        inst1.setName("gruppo1");
-        inst1.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
-        list.add(inst1);
+  @Test
+  void getGroupsFiltered() {
+    // GIVEN
+    String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
+    List<UserGroupResourceDto> list = new ArrayList<>();
+    UserGroupResourceDto inst1 = new UserGroupResourceDto();
+    inst1.setId(id);
+    inst1.setName("gruppo1");
+    inst1.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
+    list.add(inst1);
 
-        UserGroupResourceDto inst2 = new UserGroupResourceDto();
-        inst2.setId(id+"2");
-        inst2.setName("gruppo2");
-        inst2.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
-        list.add(inst2);
+    UserGroupResourceDto inst2 = new UserGroupResourceDto();
+    inst2.setId(id + "2");
+    inst2.setName("gruppo2");
+    inst2.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
+    list.add(inst2);
 
-        UserGroupResourceDto inst3 = new UserGroupResourceDto();
-        inst3.setId(id+"3");
-        inst3.setName("gruppo3");
-        inst3.setStatus(UserGroupResourceDto.StatusEnum.SUSPENDED);
-        list.add(inst3);
+    PageOfUserGroupResourceDto response = new PageOfUserGroupResourceDto();
+    response.setContent(list);
 
-        PageOfUserGroupResourceDto response = new PageOfUserGroupResourceDto();
-        response.setContent(list);
+    Mockito.when(selfcareUserGroupClient.getUserGroups(id)).thenReturn(Mono.just(response));
 
-        Mockito.when(selfcareUserGroupClient.getUserGroups(id)).thenReturn(Mono.just(response));
-
-        // WHEN
-        List<PaGroupDto> res = service.getGroups(id,id,List.of(id, id+"3"), PaGroupStatusDto.ACTIVE).collectList().block();
+    // WHEN
+    List<PaGroupDto> res = service.getGroups(id, id, List.of(id), null).collectList().block();
 
 
-        //THEN
-        assertNotNull(res);
-        assertEquals(1, res.size());
-        assertEquals(inst1.getName(), res.get(0).getName());
-    }
+    // THEN
+    assertNotNull(res);
+    assertEquals(1, res.size());
+    assertEquals(inst1.getName(), res.get(0).getName());
+  }
+
+  @Test
+  void getGroupsFilteredActive() {
+    // GIVEN
+    String id = "d0d28367-1695-4c50-a260-6fda526e9aab";
+    List<UserGroupResourceDto> list = new ArrayList<>();
+    UserGroupResourceDto inst1 = new UserGroupResourceDto();
+    inst1.setId(id);
+    inst1.setName("gruppo1");
+    inst1.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
+    list.add(inst1);
+
+    UserGroupResourceDto inst2 = new UserGroupResourceDto();
+    inst2.setId(id + "2");
+    inst2.setName("gruppo2");
+    inst2.setStatus(UserGroupResourceDto.StatusEnum.ACTIVE);
+    list.add(inst2);
+
+    UserGroupResourceDto inst3 = new UserGroupResourceDto();
+    inst3.setId(id + "3");
+    inst3.setName("gruppo3");
+    inst3.setStatus(UserGroupResourceDto.StatusEnum.SUSPENDED);
+    list.add(inst3);
+
+    PageOfUserGroupResourceDto response = new PageOfUserGroupResourceDto();
+    response.setContent(list);
+
+    Mockito.when(selfcareUserGroupClient.getUserGroups(id)).thenReturn(Mono.just(response));
+
+    // WHEN
+    List<PaGroupDto> res = service.getGroups(id, id, List.of(id, id + "3"), PaGroupStatusDto.ACTIVE)
+        .collectList().block();
+
+
+    // THEN
+    assertNotNull(res);
+    assertEquals(1, res.size());
+    assertEquals(inst1.getName(), res.get(0).getName());
+  }
 }
