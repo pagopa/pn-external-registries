9ff155997eb21e25cf56a733562d869aa1f09569
Fabrizio.D'Ammassa <fabrizio.dammassa@pagopa.it>
2023-03-09 17:21:20 +0100

    PN-4492: codegen integration

---
+++ scripts/openapi/README.md
@@ -0,0 +1,7 @@
+## PN-CODEGEN
+
+Posizionarsi nella root del progetto ed eseguire lo script:
+
+`./scripts/openapi/internal-to-external.sh -t <tag>`
+
+Dove `tag` è il tag dell'immagine di pn-codegen nel docker registry di Github; il default è `main`.
--- scripts/openapi/internal-to-external.sh
+++ scripts/openapi/internal-to-external.sh
@@ -1,9 +1,65 @@
-cat docs/openapi/pn-payment-info-internal-v1.yaml \
-    | grep -v "# NO EXTERNAL" \
-    | sed -e '/# ONLY EXTERNAL/s/^#//' \
-    > docs/openapi/pn-payment-info-external-v1.yaml
+#!/usr/bin/env bash
+    
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
 
-cat docs/openapi/pn-selfcare-internal-v1.yaml \
-    | grep -v "# NO EXTERNAL" \
-    | sed -e '/# ONLY EXTERNAL/s/^#//' \
-    > docs/openapi/pn-selfcare-external-v1.yaml
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+usage() {
+      cat <<EOF
+    Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-t <tag>]
+
+    [-h]                      : this help message
+    [-v]                      : verbose mode
+    -t <tag>                  : pn-codegen tag, default to "main"
+    
+EOF
+  exit 1
+}
+
+parse_params() {
+  # default values of variables set from params
+  tag="main"
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    -t | --tag) 
+      tag="${2-}"
+      shift
+      ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  args=("$@")
+
+  # check required params and arguments
+  [[ -z "${tag-}" ]] && usage 
+  return 0
+}
+
+dump_params(){
+  echo ""
+  echo "######      PARAMETERS      ######"
+  echo "##################################"
+  echo "Tag:          ${tag}"
+}
+
+
+# START SCRIPT
+
+parse_params "$@"
+dump_params
+
+current_dir=$(dirname ${BASH_SOURCE[0]})
+echo ${current_dir}
+cd ${current_dir}
+cd ../..
+docker run --rm -it -v ${PWD}:/usr/local/app/microsvc --name=pn-codegen ghcr.io/pagopa/pn-codegen:${tag}
