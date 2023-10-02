e48e7071349de259b1752cf50ac89734f5992526
EmanueleBonofiglio <emanuele.bonofiglio@grupposcai.it>
2023-10-02 16:09:50 +0200

    PN-8062 modifica entity e indice

--- src/main/java/it/pagopa/pn/external/registries/middleware/db/entities/OnboardInstitutionEntity.java
+++ src/main/java/it/pagopa/pn/external/registries/middleware/db/entities/OnboardInstitutionEntity.java
@@ -31,6 +31,9 @@ public class OnboardInstitutionEntity {
     private static final String COL_IPACODE = "ipaCode";
     private static final String COL_SDICODE = "sdiCode";
 
+    public static final String COL_ROOT_ID = "rootId";
+    private static final String COL_ONLY_ROOT_STATUS = "onlyRootStatus";
+
 
     @DynamoDbIgnore
     public String getInstitutionId(){
@@ -43,8 +46,11 @@ public class OnboardInstitutionEntity {
     }
 
     @Setter @Getter(onMethod=@__({@DynamoDbPartitionKey, @DynamoDbAttribute(COL_PK)}))  private String pk;
-    @Setter @Getter(onMethod=@__({@DynamoDbSecondaryPartitionKey(indexNames = { GSI_INDEX_LASTUPDATE}), @DynamoDbAttribute(COL_STATUS)}))  private String status;
+    @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_STATUS)}))  private String status;
     @Setter @Getter(onMethod=@__({@DynamoDbSecondarySortKey(indexNames = { GSI_INDEX_LASTUPDATE }), @DynamoDbAttribute(COL_LASTUPDATE)}))  private Instant lastUpdate;
+    @Setter @Getter(onMethod=@__({@DynamoDbSecondaryPartitionKey(indexNames = { GSI_INDEX_LASTUPDATE}), @DynamoDbAttribute(COL_ONLY_ROOT_STATUS)}))  private String onlyRootStatus;
+
+    @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_ROOT_ID)}))  private String rootId;
 
     @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_CREATED), @DynamoDbUpdateBehavior(UpdateBehavior.WRITE_IF_NOT_EXISTS)}))  private Instant created;
     @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_DESCRIPTION)}))  private String description;
@@ -55,4 +61,7 @@ public class OnboardInstitutionEntity {
     @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_DIGITALADDRESS)}))  private String digitalAddress;
     @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_IPACODE)}))  private String ipaCode;
     @Setter @Getter(onMethod=@__({@DynamoDbAttribute(COL_SDICODE)}))  private String sdiCode;
+
+
+
 }
