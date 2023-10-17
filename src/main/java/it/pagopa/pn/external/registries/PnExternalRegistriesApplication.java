7c9c7d0610073be23f0c582d1c2802d44597c349
Vincenzo.Veniero <vincenzo.veniero@Host-007.homenet.telecomitalia.it>
2023-10-17 17:26:24 +0200

    PN-8260: Wire Tap log variable

--- src/main/java/it/pagopa/pn/external/registries/PnExternalRegistriesApplication.java
+++ src/main/java/it/pagopa/pn/external/registries/PnExternalRegistriesApplication.java
@@ -1,10 +1,9 @@
 package it.pagopa.pn.external.registries;
 
-import it.pagopa.pn.commons.configs.listeners.ApplicationConfigurationListener;
+import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.logging.LoggingApplicationListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +14,7 @@ public class PnExternalRegistriesApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(PnExternalRegistriesApplication.class);
-        app.addListeners(new ApplicationConfigurationListener());
+        app.addListeners(new TaskIdApplicationListener());
         app.run(args);
     }
 
