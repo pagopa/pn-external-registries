b797696965594caf5e6e6d848d5bc0eef2705293
Simone Rondinella <simone.rondinella@pagopa.it>
2023-10-17 11:15:21 +0200

    PN-8171 init modifiche

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -1,3 +1,16 @@
+echo "### CREATE QUEUES ###"
+
+queues="local-delivery-push-to-ext-registries"
+
+for qn in  $( echo $queues | tr " " "\n" ) ; do
+    echo creating queue $qn ...
+
+    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
+        sqs create-queue \
+        --attributes '{"DelaySeconds":"2"}' \
+        --queue-name $qn
+done
+
 echo "### CREATE PS ###"
 
 aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
