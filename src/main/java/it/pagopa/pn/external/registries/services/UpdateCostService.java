b8fefd7af3102e3fe2a1fd82516dd02dab15ad2c
Marco Iannaccone <marco.iannaccone@pagopa.it>
2023-10-20 10:45:41 +0200

    PN-8163: latest PR review request

--- src/main/java/it/pagopa/pn/external/registries/services/UpdateCostService.java
+++ src/main/java/it/pagopa/pn/external/registries/services/UpdateCostService.java
@@ -29,22 +29,11 @@ public class UpdateCostService {
     }
 
     public Mono<UpdateCostResponseInt> updateCost(String creditorTaxId, String noticeCode, int notificationCost,
-                                                  String updateCostPhase, Instant  eventTimestamp, Instant eventStorageTimestamp) {
+                                                  String updateCostPhase, Instant eventTimestamp, Instant eventStorageTimestamp) {
 
         String iuv = creditorTaxId + noticeCode;
         String requestId = creditorTaxId + "_" + noticeCode + "_" + updateCostPhase + "_" + UUID.randomUUID();
         Instant communicationTimestamp = Instant.now();
-
-        CostUpdateResultRequestInt communicationResult = new CostUpdateResultRequestInt();
-        communicationResult.setCreditorTaxId(creditorTaxId);
-        communicationResult.setNoticeCode(noticeCode);
-        communicationResult.setUpdateCostPhase(CostUpdateCostPhaseInt.valueOf(updateCostPhase));
-        communicationResult.setRequestId(requestId);
-        communicationResult.setNotificationCost(notificationCost);
-        communicationResult.setIun(iuv);
-        communicationResult.setEventTimestamp(eventTimestamp);
-        communicationResult.setEventStorageTimestamp(eventStorageTimestamp);
-        communicationResult.setCommunicationTimestamp(communicationTimestamp);
 
         // log, including passed information and requestId
         log.info("Updating the cost on GPD: iuv: {}, creditorTaxId: {}, noticeCode: {}, requestId: {}, notificationCost: {}",
@@ -52,7 +41,6 @@ public class UpdateCostService {
 
         return gpdClient.setNotificationCost(creditorTaxId, noticeCode, requestId, (long)notificationCost)
                 .flatMap(response -> {
-                    communicationResult.setStatusCode(response.getStatusCodeValue());
 
                     PaymentsModelResponse paymentsModelResponse = getPaymentsModelResponseAndCleanUp(response);
                     // convert to JSON
@@ -64,19 +52,43 @@ public class UpdateCostService {
                         log.error("Error converting paymentsModelResponse to JSON: {}", e.getMessage());
                     }
 
-                    communicationResult.setJsonResponse(jsonResponse);
+                    CostUpdateResultRequestInt costUpdateResultRequestInt = getCostUpdateResultRequest(creditorTaxId, noticeCode, notificationCost,
+                            updateCostPhase, eventTimestamp, eventStorageTimestamp, communicationTimestamp, requestId, iuv,
+                            response.getStatusCodeValue(), jsonResponse);
 
-                    return createUpdateCostResponse(communicationResult, creditorTaxId, noticeCode);
+                    return createUpdateCostResponse(costUpdateResultRequestInt, creditorTaxId, noticeCode);
                 })
                 .onErrorResume(WebClientResponseException.class, error -> {
                     log.info("Error calling GPD: {}, iuv: {}, creditorTaxId: {}, noticeCode: {}, requestId: {}, notificationCost: {}",
                             error.getResponseBodyAsString(), iuv, creditorTaxId, noticeCode, requestId, notificationCost);
 
-                    communicationResult.setStatusCode(error.getRawStatusCode());
-                    communicationResult.setJsonResponse(error.getResponseBodyAsString());
+                    CostUpdateResultRequestInt costUpdateResultRequestInt = getCostUpdateResultRequest(creditorTaxId, noticeCode, notificationCost,
+                            updateCostPhase, eventTimestamp, eventStorageTimestamp, communicationTimestamp, requestId, iuv,
+                            error.getRawStatusCode(), error.getResponseBodyAsString());
 
-                    return createUpdateCostResponse(communicationResult, creditorTaxId, noticeCode);
+                    return createUpdateCostResponse(costUpdateResultRequestInt, creditorTaxId, noticeCode);
                 });
+    }
+
+    private CostUpdateResultRequestInt getCostUpdateResultRequest(String creditorTaxId, String noticeCode, int notificationCost,
+                                                                  String updateCostPhase, Instant eventTimestamp, Instant eventStorageTimestamp,
+                                                                  Instant communicationTimestamp, String requestId, String iuv, int statusCode,
+                                                                  String jsonResponse) {
+        CostUpdateResultRequestInt costUpdateResultRequestInt = new CostUpdateResultRequestInt();
+
+        costUpdateResultRequestInt.setCreditorTaxId(creditorTaxId);
+        costUpdateResultRequestInt.setNoticeCode(noticeCode);
+        costUpdateResultRequestInt.setUpdateCostPhase(CostUpdateCostPhaseInt.valueOf(updateCostPhase));
+        costUpdateResultRequestInt.setRequestId(requestId);
+        costUpdateResultRequestInt.setNotificationCost(notificationCost);
+        costUpdateResultRequestInt.setIun(iuv);
+        costUpdateResultRequestInt.setEventTimestamp(eventTimestamp);
+        costUpdateResultRequestInt.setEventStorageTimestamp(eventStorageTimestamp);
+        costUpdateResultRequestInt.setCommunicationTimestamp(communicationTimestamp);
+        costUpdateResultRequestInt.setStatusCode(statusCode);
+        costUpdateResultRequestInt.setJsonResponse(jsonResponse);
+
+        return costUpdateResultRequestInt;
     }
 
     private PaymentsModelResponse getPaymentsModelResponseAndCleanUp(ResponseEntity<PaymentsModelResponse> response) {
