6544bd3c194a0219258c4397f0cfe3e1a7f8a09d
Simone Rondinella <simone.rondinella@pagopa.it>
2023-10-20 10:17:23 +0200

    PN-8160 Modifiche GPD client

--- src/main/java/it/pagopa/pn/external/registries/middleware/msclient/common/OcpBaseClient.java
+++ src/main/java/it/pagopa/pn/external/registries/middleware/msclient/common/OcpBaseClient.java
@@ -7,11 +7,18 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Slf4j
 public abstract class OcpBaseClient extends CommonBaseClient {
     private static final String HEADER_API_KEY = "Ocp-Apim-Subscription-Key";
-
+    private static final String HEADER_REQUEST_ID = "X-Request-Id";
+    
     protected WebClient.Builder initWebClient(WebClient.Builder builder, String apiKey){
 
         return super.enrichBuilder(builder)
                 .defaultHeader(HEADER_API_KEY, apiKey);
     }
 
+    protected WebClient.Builder initWebClient(WebClient.Builder builder, String apiKey, String requestId){
+
+        return super.enrichBuilder(builder)
+                .defaultHeader(HEADER_API_KEY, apiKey)
+                .defaultHeader(HEADER_REQUEST_ID, requestId);
+    }
 }
