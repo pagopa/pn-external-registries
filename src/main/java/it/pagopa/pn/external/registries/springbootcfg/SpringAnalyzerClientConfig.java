327ad029f990488d31885c07e2f7d4f99e8732a3
Vincenzo.Veniero <vincenzo.veniero@Host-007.homenet.telecomitalia.it>
2023-10-24 17:35:19 +0200

    PN-7905: enable SpringAnalyzer

--- src/main/java/it/pagopa/pn/external/registries/springbootcfg/SpringAnalyzerClientConfig.java
+++ src/main/java/it/pagopa/pn/external/registries/springbootcfg/SpringAnalyzerClientConfig.java
@@ -1,9 +1,6 @@
 package it.pagopa.pn.external.registries.springbootcfg;
 
 import it.pagopa.pn.commons.configs.SpringAnalyzerConfiguration;
-import it.pagopa.tech.lollipop.consumer.assertion.client.simple.AssertionSimpleClientConfig;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
