c144c28e85724c73ddac627a9c3e97b8c89d2daf
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-08-23 18:56:43 +0200

    [PN-11934] Add unit tests

--- src/test/java/it/pagopa/pn/external/registries/middleware/msclient/SelfcarePaInstitutionClientTest.java
+++ src/test/java/it/pagopa/pn/external/registries/middleware/msclient/SelfcarePaInstitutionClientTest.java
@@ -7,6 +7,7 @@ import it.pagopa.pn.commons.exceptions.PnInternalException;
 import it.pagopa.pn.external.registries.MockAWSObjectsTestConfig;
 import it.pagopa.pn.external.registries.generated.openapi.msclient.selfcare.v2.dto.InstitutionResourceDto;
 import it.pagopa.pn.external.registries.generated.openapi.msclient.selfcare.v2.dto.ProductResourceDto;
+import it.pagopa.pn.external.registries.generated.openapi.msclient.selfcare.v2.dto.UserInstitutionResourceDto;
 import org.junit.jupiter.api.*;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.integration.ClientAndServer;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -171,5 +173,65 @@ class SelfcarePaInstitutionClientTest extends MockAWSObjectsTestConfig {
         Assertions.assertNotNull(response);
         Assertions.assertEquals(1, response.size());
         Assertions.assertEquals(response.get(0).getAddress(), institutionResourceDto.getAddress());
+    }
+
+    @Test
+    void getUserInstitutionKO() {
+        int status = 200;
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerFor(new TypeReference<List<InstitutionResourceDto>>(){});
+
+        MockServerClient mockServerClient = new MockServerClient("localhost", 9999);
+        mockServerClient.reset();
+        mockServerClient.when(request()
+                        .withMethod("GET")
+                        .withPath("/users"))
+                .respond(response().withStatusCode(500));
+
+        //When
+        try {
+            client.getInstitutions("1a2qp213-f1cb-4021-b3d0-5241216a0633").collectList().block();
+        }
+        catch (Exception e) {
+            status = ((PnInternalException) e).getStatus();
+        }
+
+        //Then
+        Assertions.assertEquals(500, status);
+    }
+
+    @Test
+    void getUserInstitutionOk() {
+        UserInstitutionResourceDto resourceDto = new UserInstitutionResourceDto();
+        resourceDto.setInstitutionId("id");
+        resourceDto.setUserId("1a2qp213-f1cb-4021-b3d0-5241216a0622");
+
+        List<UserInstitutionResourceDto> list = new ArrayList<>();
+        list.add(resourceDto);
+
+        byte[] responseBodyBites = new byte[0];
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerFor(new TypeReference<List<InstitutionResourceDto>>(){});
+        try {
+            responseBodyBites = mapper.writeValueAsBytes(list);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        MockServerClient mockServerClient = new MockServerClient("localhost", 9999);
+        mockServerClient.reset();
+        mockServerClient.when(request()
+                        .withMethod("GET")
+                        .withPath("/users"))
+                .respond(response()
+                        .withBody(responseBodyBites)
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withStatusCode(200));
+
+        //When
+        StepVerifier.create(client.getUserInstitutions("1a2qp213-f1cb-4021-b3d0-5241216a0622"))
+                .expectNextCount(1)
+                        .verifyComplete();
     }
 }
