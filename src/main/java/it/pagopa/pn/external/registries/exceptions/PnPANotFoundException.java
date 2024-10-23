4352176e7231fe5a673e4cf7108501ccd3b79851
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-10-23 10:43:19 +0200

    fix for sonar

--- src/main/java/it/pagopa/pn/external/registries/exceptions/PnPANotFoundException.java
+++ src/main/java/it/pagopa/pn/external/registries/exceptions/PnPANotFoundException.java
@@ -1,8 +1,5 @@
 package it.pagopa.pn.external.registries.exceptions;
 
-import static it.pagopa.pn.external.registries.exceptions.PnExternalregistriesExceptionCodes.ERROR_CODE_EXTERNALREGISTRIES_DIGITALDOMICILENOTFOUND;
-
-
 public class PnPANotFoundException extends PnNotFoundException {
 
     public PnPANotFoundException(String code) {
