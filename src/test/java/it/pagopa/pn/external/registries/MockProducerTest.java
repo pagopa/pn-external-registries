17d36734e1888f6cd4670dde18bc55a2af83f6e2
empassaro <emanuele.passaro@emeal.nttdata.com>
2026-03-10 13:11:03 +0100

    [PN-17542] fix: removed unused unit test

--- src/test/java/it/pagopa/pn/external/registries/MockProducerTest.java
+++ src/test/java/it/pagopa/pn/external/registries/MockProducerTest.java
@@ -2,7 +2,6 @@ package it.pagopa.pn.external.registries;
 
 import it.pagopa.pn.external.registries.middleware.queue.io.producer.sqs.SqsIOSentMessageProducer;
 import it.pagopa.pn.external.registries.middleware.queue.producer.sqs.SqsNotificationPaidProducer;
-import org.junit.jupiter.api.Test;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 public class MockProducerTest {
@@ -11,8 +10,4 @@ public class MockProducerTest {
 
     @MockitoBean
     protected SqsNotificationPaidProducer sqsNotificationPaidProducer;
-
-    @Test
-    void testDoNothing() {
-    }
 }
