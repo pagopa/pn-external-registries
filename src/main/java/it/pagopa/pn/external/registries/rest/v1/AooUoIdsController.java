d4e88c259f06f874f6f55d743ef6f574189815f8
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-05-20 16:50:14 +0200

    [PN-19036] feat: fix

--- src/main/java/it/pagopa/pn/external/registries/rest/v1/AooUoIdsController.java
+++ src/main/java/it/pagopa/pn/external/registries/rest/v1/AooUoIdsController.java
@@ -3,10 +3,6 @@ package it.pagopa.pn.external.registries.rest.v1;
 import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.api.AooUoIdsApi;
 import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.dto.FilteredPaIdsResponseDto;
 import it.pagopa.pn.external.registries.services.InfoSelfcareInstitutionsService;
-import java.util.List;
-
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -23,7 +21,7 @@ public class AooUoIdsController implements AooUoIdsApi {
     private final InfoSelfcareInstitutionsService infoSelfcareInstitutionsService;
 
     @Override
-    public Mono<ResponseEntity<FilteredPaIdsResponseDto>> getFilteredAooUoIdV2Private(List<@Pattern(regexp = "^[ -~ ]*$") @Size(min = 1, max = 50) String> id, ServerWebExchange exchange) {
+    public Mono<ResponseEntity<FilteredPaIdsResponseDto>> getFilteredAooUoIdV2Private(List<String> id, ServerWebExchange exchange) {
         return infoSelfcareInstitutionsService.filterOutRootIds(id)
                 .collectList()
                 .map(ids -> ResponseEntity.ok(new FilteredPaIdsResponseDto().ids(ids)));
