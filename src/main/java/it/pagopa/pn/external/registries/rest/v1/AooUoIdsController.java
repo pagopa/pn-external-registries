dfb8e2454ad24fe41745960f75f036708c65e12b
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-05-20 12:41:57 +0200

    [PN-19036] feat: [pn-external-registries] Violazione contratto API filtro root id

--- src/main/java/it/pagopa/pn/external/registries/rest/v1/AooUoIdsController.java
+++ src/main/java/it/pagopa/pn/external/registries/rest/v1/AooUoIdsController.java
@@ -1,8 +1,12 @@
 package it.pagopa.pn.external.registries.rest.v1;
 
 import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.api.AooUoIdsApi;
+import it.pagopa.pn.external.registries.generated.openapi.server.ipa.v1.dto.FilteredPaIdsResponseDto;
 import it.pagopa.pn.external.registries.services.InfoSelfcareInstitutionsService;
 import java.util.List;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,13 @@ import reactor.core.publisher.Mono;
 public class AooUoIdsController implements AooUoIdsApi {
 
     private final InfoSelfcareInstitutionsService infoSelfcareInstitutionsService;
+
+    @Override
+    public Mono<ResponseEntity<FilteredPaIdsResponseDto>> getFilteredAooUoIdV2Private(List<@Pattern(regexp = "^[ -~ ]*$") @Size(min = 1, max = 50) String> id, ServerWebExchange exchange) {
+        return infoSelfcareInstitutionsService.filterOutRootIds(id)
+                .collectList()
+                .map(ids -> ResponseEntity.ok(new FilteredPaIdsResponseDto().ids(ids)));
+    }
 
     private static ResponseEntity<Flux<String>> apply(List<String> dto) {
         return ResponseEntity.ok(Flux.fromIterable(dto));
